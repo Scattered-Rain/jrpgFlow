cfb6418be2d04a66c7d4d1ee135a29d94e899dc0
Dude <Florian221@gmx.net>
2014-12-21 20:59:42 +0100

    Setup Gradle.

---
+++ desktop/src/com/scatteredRain/jrpgFlow/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.scatteredRain.jrpgFlow.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.scatteredRain.jrpgFlow.JrpgFlow;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JrpgFlow(), config);
+	}
+}
